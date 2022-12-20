34ae16b83466310be01b8fdd16c3cc95ac87e390
Jace77 <rasmus102030@gmail.com>
2022-12-20 19:22:29 +0100

    ALMOST FIXED

--- script.py
+++ script.py
@@ -5,21 +5,27 @@ from PySimpleGUI import WINDOW_CLOSED
 file = open('data.json', 'r')
 data = json.load(file)
 
-interface = [[sg.Listbox(values=data, size=(20, 10))],
+interface = [
+    [sg.Listbox(values=data[0], size=(20, 10))],
     [sg.InputText(key='product', size=(20, 1))],
     [sg.InputText(key='num', size=(20, 1))],
-    [sg.Button('Add'), sg.Button('Edit'), sg.Button('Close')]]
+    [sg.Button('Add'), sg.Button('Edit'), sg.Button('Close')]
+]
 
 window = sg.Window('Warehouse Contents', interface, margins=(100, 100))
 window.read()
-output=open('data.json', 'w')
+
 while True:
     event, values = window.read()
     if event == 'Add':
-        data[0][values['product']]=values['num']
-        output.write('data')
+        output = open('data.json', 'w')
+        data[0][values['product']] = values['num']
+        output.write(json.dumps(data))
+        output.close()
     if event == 'Edit':
+        output = open('data.json', 'w')
         data[0][values[[0]]] = values['num']
-        json.dump(data, output)
+        output.write(json.dumps(data))
+        output.close()
     if event == 'Close' or event == WINDOW_CLOSED:
         break
