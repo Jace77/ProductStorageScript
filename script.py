d1f7dadc3dd36d79f15b526628639f2d940ea184
Jace77 <rasmus102030@gmail.com>
2022-12-20 17:21:29 +0100

    Update script.py

--- script.py
+++ script.py
@@ -7,14 +7,16 @@ data = json.load(file)
 interface = [[sg.Listbox(values=data, size=(20, 10))],
     [sg.InputText(key='product', size=(20, 1))],
     [sg.InputText(key='num', size=(20, 1))],
-    [sg.Button('Save'), sg.Button('Close')]]
+    [sg.Button('Add'), sg.Button('Edit'), sg.Button('Close')]]
 
 window = sg.Window('Warehouse Contents', interface, margins=(100, 100))
 window.read()
 
 while True:
     event, values = window.read()
-    if event =='Close':
+    if event =='Close' or event == sg.WINDOW_CLOSED():
         break
-    if event == 'Save' or event == sg.WINDOW_CLOSED():
+    if event == 'Add':
         data[0][values['product']]=values['num']
+    if event == 'Edit':
+        data[0][values[[0]]] = values['num']
